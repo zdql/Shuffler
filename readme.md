7e09023b6f674471209d3c2ad8f3b9a550f3ca21
pbr48 <pbr48@cornell.edu>
2023-11-19 15:38:17 -0500

    feat: initial commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+# MP3 Shuffle Player
+
+This is a simple Python script to shuffle play MP3 files from a specified folder. It's a command-line tool that randomly plays all the MP3 files located in a given directory.
+
+Prerequisites
+Before you run the script, make sure you have:
+
+Python: The script is written in Python and requires it to run. You can download and install Python from python.org.
+
+Pygame Library: This script uses the pygame library for audio playback. If you haven't installed pygame, you can do so using pip:
+
+bash
+Copy code
+pip install pygame
+Installation
+No installation is required. Just download the shuffle_play.py script to your local machine.
+
+Usage
+To use the script, follow these steps:
+
+Place Your MP3 Files: Ensure your MP3 files are in a folder.
+
+Run the Script: Open a terminal or command prompt and navigate to the directory where you have saved shuffle_play.py. Run the script with the following command:
+
+bash
+Copy code
+python shuffle_play.py path_to_your_music_folder
+Replace path_to_your_music_folder with the actual path to your folder containing MP3 files.
+
+Enjoy the Music: The script will shuffle and play the MP3 files in the specified folder.
+
+Features
+Shuffle Play: Randomly plays all the MP3 files in the specified folder.
+Command Line Interface: Easy to run from the terminal or command prompt.
+Troubleshooting
+Pygame Installation: If you encounter issues with pygame, ensure it is correctly installed and updated.
+MP3 Files Only: The script is designed to play MP3 files only. Make sure your folder contains MP3 files.
+Correct Folder Path: If the script cannot find the folder, check if the provided path is correct.
+License
+This script is released under the MIT License.
---
+++ shuffle_play.py
@@ -0,0 +1,63 @@
+import os
+import random
+import pygame
+import sys
+import time
+
+
+def play_music(music_file):
+    """
+    Stream music with mixer.music module in a blocking manner.
+    This will stream the sound from disk while playing.
+    """
+    # set up the mixer
+    freq = 44100     # audio CD quality
+    bitsize = -16    # unsigned 16 bit
+    channels = 2     # 1 is mono, 2 is stereo
+    buffer = 2048    # number of samples (experiment to get best sound)
+    pygame.mixer.init(freq, bitsize, channels, buffer)
+
+    # volume value 0.0 to 1.0
+    pygame.mixer.music.set_volume(0.8)
+
+    # Try to load the music, raise an exception if it fails
+    try:
+        pygame.mixer.music.load(music_file)
+    except pygame.error:
+        print(f"File {music_file} not found! ({pygame.get_error()})")
+        return
+
+    pygame.mixer.music.play()
+
+
+def main(folder_path):
+    """
+    Entry point of the program
+    """
+    # check if the music directory exists
+    if not os.path.isdir(folder_path):
+        print(f"Directory '{folder_path}' not found!")
+        return
+
+    # get all mp3 files from the directory
+    mp3_files = [f for f in os.listdir(folder_path) if f.endswith('.mp3')]
+
+    # shuffle the list of mp3 files
+    random.shuffle(mp3_files)
+
+    # play each song in the shuffled list
+    for file in mp3_files:
+        file_path = os.path.join(folder_path, file)
+        print(f"Playing {file}...")
+        play_music(file_path)
+
+        # check if playback has finished
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python shuffle_play.py <path_to_music_folder>")
+    else:
+        main(sys.argv[1])
